656213c968500466a8e5276ab3363c8dab6f4506
Wang Qiang <37444407+XDUWQ@users.noreply.github.com>
2021-12-05 16:12:10 +0800

    Update sketch_rnn.py

--- sketch_rnn.py
+++ sketch_rnn.py
@@ -439,7 +439,3 @@ if __name__ == "__main__":
     for epoch in range(50001):
         model.train(epoch)
 
-    '''
-    model.load('encoder.pth','decoder.pth')
-    model.conditional_generation(0)
-    #'''
